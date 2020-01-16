ba624fb19fc2a04a4b648920864031e871e43f71
Devan Sisson <devanslink@gmail.com>
2020-01-16 13:15:49 -0700

    remedy vim install

--- install-vim.sh
+++ install-vim.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 source $PWD/scripts/helpers.sh
 
+link_dot_file(){
+  print "Linking $1"
+  ln -s $PWD/files/$1 $HOME/.$1
+}
+
 # verify git is installed
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
 
